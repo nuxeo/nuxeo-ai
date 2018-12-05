22b1d9a308e5ce2131185e32b3a46791ed7b18df
Gethin James <gjames@nuxeo.com>
2018-12-05 11:15:42 +0100

    NXP-26375: Add a primary type filter

---
+++ nuxeo-ai-pipes/src/main/java/org/nuxeo/ai/pipes/filters/PrimaryTypeFilter.java
@@ -0,0 +1,51 @@
+/*
+ * (C) Copyright 2018 Nuxeo (http://nuxeo.com/) and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     Gethin James
+ */
+package org.nuxeo.ai.pipes.filters;
+
+import java.util.List;
+import java.util.Map;
+import org.nuxeo.ai.pipes.streams.Initializable;
+import org.nuxeo.ecm.core.api.DocumentModel;
+
+/**
+ * A Document type filter.
+ * Check that none match the excluded types and is of the specified type
+ */
+public class PrimaryTypeFilter implements Filter.DocumentFilter, Initializable {
+
+    protected String isType;
+
+    protected List<String> excludedTypes;
+
+    @Override
+    public void init(Map<String, String> options) {
+        isType = options.get("isType");
+        excludedTypes = propsList(options.get("excludedTypes"));
+    }
+
+    /**
+     * A predicate that checks that none match the excluded types and is of the specified type
+     */
+    @Override
+    public boolean test(DocumentModel doc) {
+        return excludedTypes.stream().noneMatch(t -> t.equals(doc.getType()))
+                && (isType == null || isType.equals(doc.getType()));
+    }
+
+}
