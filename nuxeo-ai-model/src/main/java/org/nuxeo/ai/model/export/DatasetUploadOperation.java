bdfd2bd29d7486be0daf924a87657b2b7997fdce
Gethin James <gjames@nuxeo.com>
2019-01-23 16:37:25 +0100

    NXP-26636: Add a DatasetUploadOperation

---
+++ nuxeo-ai-model/src/main/java/org/nuxeo/ai/model/export/DatasetUploadOperation.java
@@ -0,0 +1,81 @@
+/*
+ * (C) Copyright 2019 Nuxeo (http://nuxeo.com/) and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     Gethin James
+ */
+package org.nuxeo.ai.model.export;
+
+import static org.nuxeo.ai.model.AiDocumentTypeConstants.CORPUS_TYPE;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.nuxeo.ai.cloud.CloudClient;
+import org.nuxeo.ecm.automation.core.Constants;
+import org.nuxeo.ecm.automation.core.annotations.Context;
+import org.nuxeo.ecm.automation.core.annotations.Operation;
+import org.nuxeo.ecm.automation.core.annotations.OperationMethod;
+import org.nuxeo.ecm.automation.core.annotations.Param;
+import org.nuxeo.ecm.core.api.CoreSession;
+import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.core.api.DocumentRef;
+
+@Operation(id = DatasetUploadOperation.ID, category = Constants.CAT_SERVICES, label = "Upload a dataset",
+        description = "Uploads the dataset specified in a corpus document.")
+public class DatasetUploadOperation {
+
+    public static final String ID = "AI.DatasetUpload";
+
+    private static final Logger log = LogManager.getLogger(DatasetUploadOperation.class);
+
+    @Context
+    public CoreSession session;
+
+    @Context
+    protected CloudClient client;
+
+    @Param(name = "document", description = "A corpus document", required = false)
+    protected DocumentModel documentModel;
+
+    @OperationMethod
+    public void run(DocumentModel document) {
+        if (session.getPrincipal().isAdministrator()) {
+            if (document != null && CORPUS_TYPE.equals(document.getType())) {
+                if (client.isAvailable()) {
+                    log.info("Uploading dataset to cloud for corpus doc {}", document.getId());
+                    boolean success = client.uploadedDataset(document);
+                    log.info("Upload of dataset to cloud for corpus doc {} {}.", document.getId(),
+                             success ? "successful" : "failed");
+                } else {
+                    log.warn("Upload to cloud not possible for corpus doc {}, type {} and client {}",
+                             document.getId(), document.getType(), client.isAvailable());
+                }
+            }
+        } else {
+            log.warn("User {} is not authorised to run the {} operation.", session.getPrincipal().getPrincipalId(), ID);
+        }
+    }
+
+    @OperationMethod
+    public void run(DocumentRef docRef) {
+        DocumentModel docModel = session.getDocument(docRef);
+        run(docModel);
+    }
+
+    @OperationMethod
+    public void run() {
+        run(documentModel);
+    }
+}
