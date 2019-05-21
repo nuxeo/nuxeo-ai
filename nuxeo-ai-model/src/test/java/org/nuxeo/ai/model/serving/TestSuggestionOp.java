47b337785da89c08165c7e387a71e9adecc92c41
Gethin James <gjames@nuxeo.com>
2019-05-21 11:34:12 +0200

    NXP-27182: Add property ref content on suggestions

--- nuxeo-ai-model/src/test/java/org/nuxeo/ai/model/serving/TestSuggestionOp.java
+++ nuxeo-ai-model/src/test/java/org/nuxeo/ai/model/serving/TestSuggestionOp.java
@@ -25,13 +25,17 @@ import static org.nuxeo.ai.pipes.services.JacksonUtil.MAPPER;
 
 import java.io.IOException;
 import java.io.Serializable;
-
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
 import javax.inject.Inject;
-
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.nuxeo.ai.enrichment.EnrichmentTestFeature;
+import org.nuxeo.common.utils.Path;
 import org.nuxeo.ecm.automation.AutomationService;
 import org.nuxeo.ecm.automation.OperationChain;
 import org.nuxeo.ecm.automation.OperationContext;
@@ -39,13 +43,13 @@ import org.nuxeo.ecm.automation.OperationException;
 import org.nuxeo.ecm.automation.test.AutomationFeature;
 import org.nuxeo.ecm.core.api.CoreSession;
 import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.core.api.impl.DocumentModelImpl;
 import org.nuxeo.ecm.core.api.impl.blob.JSONBlob;
 import org.nuxeo.ecm.core.blob.BlobManager;
 import org.nuxeo.ecm.platform.test.PlatformFeature;
 import org.nuxeo.runtime.test.runner.Deploy;
 import org.nuxeo.runtime.test.runner.Features;
 import org.nuxeo.runtime.test.runner.FeaturesRunner;
-
 import com.fasterxml.jackson.databind.JsonNode;
 import com.github.tomakehurst.wiremock.junit.WireMockRule;
 
@@ -73,8 +77,12 @@ public class TestSuggestionOp {
     @Deploy("org.nuxeo.ai.ai-model:OSGI-INF/cloud-client-test.xml")
     public void shouldCall() throws OperationException, IOException {
 
+        DocumentModel referencedDoc = new DocumentModelImpl(null, "File", "123456", new Path("referenced doc"), null, null,
+                                                            null, null, null, null, null);
+        session.importDocuments(Arrays.asList(referencedDoc));
+
         String title = "My document suggestion";
-        DocumentModel testDoc = session.createDocumentModel("/", "My Doc", "File");
+        DocumentModel testDoc = session.createDocumentModel("/", "My Doc", "FileRefDoc");
         testDoc.setPropertyValue("dc:title", title);
         testDoc.setPropertyValue("file:content", (Serializable) createTestBlob(manager));
         testDoc = session.createDocument(testDoc);
@@ -110,7 +118,29 @@ public class TestSuggestionOp {
 
     protected void assertSuggestionList(JSONBlob blob) throws IOException {
         JsonNode jsonTree = MAPPER.readTree(blob.getString());
-        assertTrue(jsonTree.get(0).get("suggestions").size() > 0);
+        JsonNode suggestions = jsonTree.get(0).get("suggestions");
+        assertEquals(4, suggestions.size());
+        for (JsonNode suggestion : suggestions) {
+            List<JsonNode> asProperty = suggestion.get("values").findValues("value");
+            Set<String> entityType = asProperty.stream()
+                                               .map(jsonNode -> jsonNode.get("entity-type"))
+                                               .filter(Objects::nonNull)
+                                               .map(JsonNode::asText)
+                                               .collect(Collectors.toSet());
+            switch (suggestion.get("property").asText()) {
+                case "dr:docIdOnlyRef":
+                    assertTrue(entityType.contains("document"));
+                    break;
+                case "dc:creator":
+                    assertTrue(entityType.contains("user"));
+                    break;
+                case "dc:nature":
+                    assertTrue(entityType.contains("directoryEntry"));
+                    break;
+                case "dc:subjects":
+                    assertTrue(entityType.contains("directoryEntry"));
+            }
+        }
     }
 
     protected void assertEmptyList(JSONBlob blob) {
