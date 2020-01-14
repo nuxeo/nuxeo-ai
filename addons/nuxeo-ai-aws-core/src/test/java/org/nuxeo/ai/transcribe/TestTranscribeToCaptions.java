3c6b2351002d3e6f4ee232a99e04a1b6cb41b9de
Andrei Nechaev <anechaev@nuxeo.com>
2020-01-14 15:24:45 -0500

    NXP-28125: caption service

---
+++ addons/nuxeo-ai-aws-core/src/test/java/org/nuxeo/ai/transcribe/TestTranscribeToCaptions.java
@@ -0,0 +1,101 @@
+/*
+ * (C) Copyright 2006-2019 Nuxeo (http://nuxeo.com/) and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *
+ * Contributors:
+ *     anechaev
+ */
+package org.nuxeo.ai.transcribe;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+import static org.junit.Assert.assertNotNull;
+import static org.nuxeo.ai.AIConstants.ENRICHMENT_FACET;
+import static org.nuxeo.ai.listeners.VideoAboutToChange.CAPTIONABLE_FACET;
+import static org.nuxeo.ai.transcribe.TranscribeWork.LANGUAGE_KEY;
+import static org.nuxeo.ai.transcribe.TranscribeWork.TRANSCRIBE_DONE_EVENT;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.Serializable;
+import java.nio.file.Files;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.inject.Inject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.nuxeo.common.utils.FileUtils;
+import org.nuxeo.ecm.core.api.Blobs;
+import org.nuxeo.ecm.core.api.CoreSession;
+import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.core.event.EventService;
+import org.nuxeo.ecm.core.event.impl.DocumentEventContext;
+import org.nuxeo.ecm.platform.test.PlatformFeature;
+import org.nuxeo.runtime.test.runner.Deploy;
+import org.nuxeo.runtime.test.runner.Features;
+import org.nuxeo.runtime.test.runner.FeaturesRunner;
+import org.nuxeo.runtime.test.runner.TransactionalFeature;
+
+@RunWith(FeaturesRunner.class)
+@Features({PlatformFeature.class})
+@Deploy("org.nuxeo.ai.ai-core")
+@Deploy("org.nuxeo.ai.aws.aws-core")
+public class TestTranscribeToCaptions {
+
+    @Inject
+    protected CoreSession session;
+
+    @Inject
+    protected EventService es;
+
+    @Inject
+    protected TransactionalFeature tf;
+
+    @Test
+    public void shouldConvertToCaptions() throws IOException {
+        DocumentModel fileDoc = session.createDocumentModel("/", "MyDoc", "File");
+        fileDoc.addFacet(CAPTIONABLE_FACET);
+        fileDoc.addFacet(ENRICHMENT_FACET);
+
+        File jsonFile = FileUtils.getResourceFileFromContext("files/transcribe_resp.json");
+        String json = new String(Files.readAllBytes(jsonFile.toPath()));
+
+        Map<String, Serializable> item = new HashMap<>();
+        item.put("model", "aws.transcribe");
+        item.put("raw", (Serializable) Blobs.createJSONBlob(json));
+
+        List<Map<String, Serializable>> items = Collections.singletonList(item);
+        fileDoc.setPropertyValue("enrichment:items", (Serializable) items);
+        fileDoc = session.createDocument(fileDoc);
+
+        assertNotNull(fileDoc);
+        session.save();
+
+        DocumentEventContext ctx = new DocumentEventContext(session, session.getPrincipal(), fileDoc);
+        ctx.setProperty(LANGUAGE_KEY, "en_US");
+        es.fireEvent(ctx.newEvent(TRANSCRIBE_DONE_EVENT));
+
+        es.waitForAsyncCompletion();
+
+        tf.nextTransaction();
+
+        DocumentModel doc = session.getDocument(fileDoc.getRef());
+        @SuppressWarnings("unchecked")
+        List<Map<String, Serializable>> prop = (List<Map<String, Serializable>>) doc.getPropertyValue("cap:captions");
+        assertThat(prop).isNotNull().hasSize(1);
+    }
+}
---
+++ nuxeo-ai-core/src/main/java/org/nuxeo/ai/services/CaptionServiceImpl.java
@@ -0,0 +1,94 @@
+/*
+ * (C) Copyright 2006-2019 Nuxeo (http://nuxeo.com/) and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *
+ * Contributors:
+ *     anechaev
+ */
+package org.nuxeo.ai.services;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.List;
+import javax.annotation.Nonnull;
+
+import org.apache.commons.lang3.time.DurationFormatUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.nuxeo.ai.metadata.Caption;
+import org.nuxeo.ecm.core.api.Blob;
+import org.nuxeo.ecm.core.api.Blobs;
+import org.nuxeo.ecm.core.api.NuxeoException;
+import org.nuxeo.runtime.api.Framework;
+
+/**
+ * Implements {@link CaptionService} to transform given captions into VTT
+ */
+public class CaptionServiceImpl implements CaptionService {
+
+    private static final Logger log = LogManager.getLogger(CaptionServiceImpl.class);
+
+    protected static final String CAPTIONS_PREFIX = "captions-";
+
+    protected static final String CAPTIONS_SUFFIX = ".vtt";
+
+    protected static final String TIME_FORMAT = "HH:mm.ss.SSS";
+
+    protected static final byte[] NO_CAPTIONS_COMMENT = "NOTE no captions available\n".getBytes();
+
+    protected static final byte[] WEBVTT_HEADER = "WEBVTT\n\n".getBytes();
+
+    public static final String TEXT_VTT_MIME_TYPE = "text/vtt";
+
+    @Override
+    public Blob write(@Nonnull List<Caption> captions) {
+        try {
+            File tempFile = Framework.createTempFile(CAPTIONS_PREFIX, CAPTIONS_SUFFIX);
+            Framework.trackFile(tempFile, captions);
+            try (FileOutputStream fos = new FileOutputStream(tempFile)) {
+                fos.write(WEBVTT_HEADER);
+
+                if (captions.isEmpty()) {
+                    fos.write(NO_CAPTIONS_COMMENT);
+                }
+
+                for (Caption cap : captions) {
+                    String timestamp = getTimestamp(cap);
+                    fos.write(timestamp.getBytes());
+
+                    for (String l : cap.getLines()) {
+                        fos.write(("- " + l + "\n").getBytes());
+                    }
+
+                    fos.write("\n".getBytes());
+                }
+            }
+
+            return Blobs.createBlob(tempFile, TEXT_VTT_MIME_TYPE);
+        } catch (IOException e) {
+            log.error(e);
+            throw new NuxeoException(e);
+        }
+
+    }
+
+    protected String getTimestamp(Caption cap) {
+        String start = DurationFormatUtils.formatDuration(cap.getStart(), TIME_FORMAT);
+        String end = DurationFormatUtils.formatDuration(cap.getEnd(), TIME_FORMAT);
+
+        return start + " --> " + end + "\n";
+    }
+}
