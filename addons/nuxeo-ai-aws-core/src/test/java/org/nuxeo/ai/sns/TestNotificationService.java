8cebbbd9edbdec904562e33001a8a81d77002279
Andrei Nechaev <anechaev@nuxeo.com>
2020-05-06 14:37:59 +0200

    AICORE-291: removing topic creation assuming sns topic is always created

--- addons/nuxeo-ai-aws-core/src/test/java/org/nuxeo/ai/sns/TestNotificationService.java
+++ addons/nuxeo-ai-aws-core/src/test/java/org/nuxeo/ai/sns/TestNotificationService.java
@@ -19,25 +19,29 @@
  */
 package org.nuxeo.ai.sns;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertNotNull;
 import static org.nuxeo.ai.rekognition.RekognitionService.DETECT_SNS_TOPIC;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import javax.inject.Inject;
 
-import org.junit.AfterClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.nuxeo.ai.AWS;
-import org.nuxeo.ai.enrichment.EnrichmentTestFeature;
 import org.nuxeo.ecm.platform.test.PlatformFeature;
-import org.nuxeo.runtime.api.Framework;
 import org.nuxeo.runtime.test.runner.Deploy;
 import org.nuxeo.runtime.test.runner.Features;
 import org.nuxeo.runtime.test.runner.FeaturesRunner;
 import org.nuxeo.runtime.test.runner.RuntimeFeature;
 
+import com.amazonaws.services.sns.AmazonSNS;
+import com.amazonaws.services.sns.model.GetTopicAttributesRequest;
+import com.amazonaws.services.sns.model.GetTopicAttributesResult;
+
 @RunWith(FeaturesRunner.class)
-@Features({EnrichmentTestFeature.class, RuntimeFeature.class, PlatformFeature.class})
+@Features({RuntimeFeature.class, PlatformFeature.class})
 @Deploy("org.nuxeo.runtime.aws")
 @Deploy("org.nuxeo.ai.aws.aws-core")
 public class TestNotificationService {
@@ -45,18 +49,24 @@ public class TestNotificationService {
     @Inject
     protected NotificationService ns;
 
-    @AfterClass
-    public static void cleanup() {
-        AWS.assumeCredentials();
-        Framework.getService(NotificationService.class).removeTopic(DETECT_SNS_TOPIC);
-    }
-
     @Test
     @Deploy("org.nuxeo.ai.aws.aws-core:OSGI-INF/test-aws-sns.xml")
-    public void shouldCreateTopic() {
+    public void shouldCreateTopic() throws URISyntaxException {
         AWS.assumeCredentials();
         assertNotNull(ns);
         String arn = ns.getTopicArnFor(DETECT_SNS_TOPIC);
         assertNotNull(arn);
+
+        AmazonSNS client = ns.getClient();
+        assertNotNull(client);
+
+        URI uri = new URI("https://not_a_path.net");
+        // should subscribe with no exceptions
+        String subArn = ns.subscribe(arn, uri);
+        assertThat(subArn).isNotBlank();
+
+        GetTopicAttributesRequest topicRequest = new GetTopicAttributesRequest(arn);
+        GetTopicAttributesResult result = client.getTopicAttributes(topicRequest);
+        assertThat(result.getAttributes()).isNotEmpty();
     }
 }
