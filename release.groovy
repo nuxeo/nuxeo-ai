9c79d79e38e76d07cb30871f9a426be3d666e3e2
Julien Carsique <jcarsique@nuxeo.com>
2020-06-29 16:31:54 +0200

    AICORE-328: release pipeline

---
+++ release.groovy
@@ -0,0 +1,60 @@
+/*
+ *
+ *  * (C) Copyright 2020 Nuxeo (http://nuxeo.com/).
+ *  * This is unpublished proprietary source code of Nuxeo SA. All rights reserved.
+ *  * Notice of copyright on this source code does not indicate publication.
+ *  *
+ *  * Contributors:
+ *  *      jcarsique
+ *
+ *
+ */
+
+void setGitHubBuildStatus(String context) {
+    step([
+            $class       : 'GitHubCommitStatusSetter',
+            reposSource  : [$class: 'ManuallyEnteredRepositorySource', url: 'https://github.com/nuxeo/nuxeo-ai'],
+            contextSource: [$class: 'ManuallyEnteredCommitContextSource', context: context],
+            errorHandlers: [[$class: 'ShallowAnyErrorHandler']]
+    ])
+}
+
+pipeline {
+    agent {
+        label "jenkins-ai-nuxeo11"
+    }
+    parameters {
+        string(name: 'BRANCH', defaultValue: 'master', description: 'Branch to release from.')
+        string(name: 'VERSION', description: '''The version to release. 
+Leave it unset to use the version of the Maven POM.
+The version must match "X.Y.Z[-PRERELEASE][+BUILD]"''')
+        choice(name: 'INCREMENT', choices: ['patch', 'minor', 'major', 'release'], description: '''Semantic versioning increment.
+The "release" increment mode removes any PRERELEASE or BUILD parts (see VERSION).''')
+        booleanParam(name: 'DRY_RUN', defaultValue: true, description: 'Dry run')
+    }
+    options {
+        disableConcurrentBuilds()
+        buildDiscarder(logRotator(numToKeepStr: '10', artifactNumToKeepStr: '3'))
+        timeout(time: 1, unit: 'HOURS')
+    }
+    stages {
+        stage('Release') {
+            steps {
+                setGitHubBuildStatus('release')
+                container('platform11') {
+                    withEnv(["BRANCH=${params.BRANCH}", "VERSION=${params.VERSION}", "INCREMENT=${params.INCREMENT}",
+                             "DRY_RUN=${params.DRY_RUN}"]) {
+                        sh './tools/release.sh'
+                        sh 'chmod +r VERSION charts/*/templates/release.yaml || true'
+                    }
+                }
+            }
+            post {
+                always {
+                    setGitHubBuildStatus('release')
+                    archiveArtifacts artifacts: 'VERSION, charts/*/templates/release.yaml', allowEmptyArchive: true
+                }
+            }
+        }
+    }
+}
