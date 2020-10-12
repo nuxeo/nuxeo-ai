abc7e8fb7756845ba6727685c411fcd1c1ed46f0
Andrei Nechaev <anechaev@nuxeo.com>
2020-10-12 14:54:23 +0200

    AICORE-359: operation obtaining instance datasource

---
+++ nuxeo-ai-model/src/main/java/org/nuxeo/ai/cloud/DatasourceOperation.java
@@ -0,0 +1,45 @@
+/*
+ * (C) Copyright 2006-2020 Nuxeo (http://nuxeo.com/) and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *
+ * Contributors:
+ *     anechaev
+ */
+package org.nuxeo.ai.cloud;
+
+import static org.nuxeo.ecm.automation.core.Constants.CAT_LOCAL_CONFIGURATION;
+
+import org.nuxeo.ecm.automation.core.annotations.Operation;
+import org.nuxeo.ecm.automation.core.annotations.OperationMethod;
+import org.nuxeo.ecm.core.api.DocumentNotFoundException;
+import org.nuxeo.runtime.api.Framework;
+
+@Operation(id = DatasourceOperation.ID, category = CAT_LOCAL_CONFIGURATION, label = "Datasource configuration", description = "Get Datasource label for the current instance")
+public class DatasourceOperation {
+
+    public static final String ID = "AI.Datasource";
+
+    @OperationMethod
+    public String run() {
+        CloudClient cloudClient = Framework.getService(CloudClient.class);
+        CloudConfigDescriptor cloudConfig = cloudClient.getCloudConfig();
+        if (cloudConfig == null) {
+            // Trick to return a 404 http code response (RestOperationContext doesn't work anymore)
+            throw new DocumentNotFoundException("Client Cloud configuration is missing");
+        }
+
+        return cloudConfig.getDatasource();
+    }
+}
