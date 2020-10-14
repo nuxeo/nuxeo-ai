e2b05ff4240e8673b8356c6a8cad9358707d359f
Vladimir Pasquier <vpasquier@nuxeo.com>
2020-10-14 17:36:02 -0400

    AICORE-355: store all keys to retrieve all descriptors

--- nuxeo-ai-core/src/main/java/org/nuxeo/ai/services/PersistedConfigurationServiceImpl.java
+++ nuxeo-ai-core/src/main/java/org/nuxeo/ai/services/PersistedConfigurationServiceImpl.java
@@ -21,7 +21,14 @@ package org.nuxeo.ai.services;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.nuxeo.ai.configuration.ThresholdConfiguratorDescriptor;
@@ -41,6 +48,12 @@ public class PersistedConfigurationServiceImpl extends DefaultComponent implemen
 
     public static final String KEY_VALUE_STORE = "aiConfigurationKVStore";
 
+    public static final String ALL_KEYS = "KEYS";
+
+    public PersistedConfigurationServiceImpl() {
+        this.register(ThresholdConfiguratorDescriptor.class);
+    }
+
     @Override
     public void register(Class<? extends Descriptor> clazz) {
         reader.getXMap().register(clazz);
@@ -50,20 +63,50 @@ public class PersistedConfigurationServiceImpl extends DefaultComponent implemen
     public void persist(String key, Descriptor contribution) throws IOException {
         XMap xmap = reader.getXMap();
         String xml = xmap.toXML(contribution);
+        this.persist(key, xml);
+    }
+
+    @Override
+    public void persist(String key, String xml) {
+        // Store descriptor
         getStore().put(key, xml);
+        // Store key reference
+        Set<String> keys = getAllKeys();
+        keys.add(key);
+        String allKeys = String.join(",", keys);
+        getStore().put(ALL_KEYS, allKeys);
     }
 
     @Override
     public Descriptor retrieve(String key) throws IOException {
         byte[] bytes = getStore().get(key);
         XMap xmap = reader.getXMap();
-        xmap.register(ThresholdConfiguratorDescriptor.class);
         Descriptor descriptor;
         try (ByteArrayInputStream bais = new ByteArrayInputStream(bytes)) {
             descriptor = (Descriptor) xmap.load(bais);
         }
-
         return descriptor;
+    }
+
+    @Override
+    public Pair<String, List<Descriptor>> retrieveAllDescriptors() throws IOException {
+        Set<String> keys = getAllKeys();
+        List<Descriptor> descriptors = new ArrayList<>();
+        String xmlPayLoad = "";
+        for (String key : keys) {
+            byte[] bytes = getStore().get(key);
+            XMap xmap = reader.getXMap();
+            xmlPayLoad.concat(new String(bytes));
+            try (ByteArrayInputStream bais = new ByteArrayInputStream(bytes)) {
+                descriptors.add((Descriptor) xmap.load(bais));
+            }
+        }
+        return new ImmutablePair<>(xmlPayLoad, descriptors);
+    }
+
+    protected Set<String> getAllKeys() {
+        String allKeys = new String(getStore().get(ALL_KEYS));
+        return new HashSet<>(Arrays.asList(allKeys.split(",")));
     }
 
     protected KeyValueStore getStore() {
