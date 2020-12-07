9df8f292b707d7d6200da3b44c40b9a18d383506
Julien Carsique <jcarsique@nuxeo.com>
2020-12-07 16:04:42 +0100

    feat(AICORE-410): align with NXP 2021
    set version 3.0.0-2021-SNAPSHOT
    use WebUI version 3.0.2-rc.49
    sync master-2021 with master
    handle master-2021 branch
    doc: fix Apache license

---
+++ tools/rebase.sh
@@ -0,0 +1,38 @@
+#!/bin/bash -xe
+#
+# Helper script to quickly sync master-2021 with master (ie: rebase while they don't diverge)
+#
+# (C) Copyright 2020 Nuxeo (http://nuxeo.com/) and others.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Contributors:
+#      jcarsique
+#
+
+hub sync || echo "WARN: 'hub' command failed but master ref-masterFor2021 master-2021 branches must be up to date with origin."
+git show -s --pretty=format:'%C(auto)%h%d' master ref-masterFor2021 master-2021
+if [[ -z $(git rev-list ref-masterFor2021..master) ]]; then
+    echo "No changes to backport from master to master-2021"
+    exit 0
+fi
+git checkout master-2021
+git rebase -Xours --onto master ref-masterFor2021 master-2021
+git update-ref refs/heads/ref-masterFor2021 master
+
+version=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout -N)
+version=${version/SNAPSHOT/2021-SNAPSHOT}
+mvn -Pinternal versions:set versions:update-child-modules versions:set-property -Dproperty=nuxeo.ai.version -DnewVersion="$version" -DprocessDependencies=false -DgenerateBackupPoms=false
+git commit -m"rebase: fix version $version" -a --no-edit ||true
+
+git push origin master-2021 ref-masterFor2021
