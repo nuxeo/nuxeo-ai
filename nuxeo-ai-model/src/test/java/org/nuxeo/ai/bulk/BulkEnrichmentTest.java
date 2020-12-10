28a31a3cf4a325c66e90fcf32ecf7b04c2b74ef1
Andrei Nechaev <anechaev@nuxeo.com>
2020-12-10 15:26:06 +0100

    AICORE-413: annotation confirm operation

--- nuxeo-ai-model/src/test/java/org/nuxeo/ai/bulk/BulkEnrichmentTest.java
+++ nuxeo-ai-model/src/test/java/org/nuxeo/ai/bulk/BulkEnrichmentTest.java
@@ -18,7 +18,6 @@
  */
 package org.nuxeo.ai.bulk;
 
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.github.tomakehurst.wiremock.junit.WireMockRule;
 import com.google.common.collect.Sets;
 import org.junit.Rule;
@@ -29,8 +28,6 @@ import org.nuxeo.ai.enrichment.EnrichmentTestFeature;
 import org.nuxeo.ai.metadata.SuggestionMetadataWrapper;
 import org.nuxeo.ai.model.export.DatasetExportService;
 import org.nuxeo.ai.pipes.types.PropertyType;
-import org.nuxeo.ecm.automation.test.AutomationFeature;
-import org.nuxeo.ecm.core.api.Blob;
 import org.nuxeo.ecm.core.api.CoreSession;
 import org.nuxeo.ecm.core.api.DocumentModel;
 import org.nuxeo.ecm.core.api.DocumentModelList;
@@ -52,7 +49,6 @@ import org.nuxeo.runtime.test.runner.TransactionalFeature;
 import javax.inject.Inject;
 import java.io.Serializable;
 import java.time.Duration;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
@@ -62,9 +58,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
-import static org.nuxeo.ai.AIConstants.AUTO_HISTORY;
 import static org.nuxeo.ai.AIConstants.ENRICHMENT_FACET;
-import static org.nuxeo.ai.AIConstants.ENRICHMENT_SCHEMA_NAME;
 import static org.nuxeo.ai.adapters.DatasetExport.DATASET_EXPORT_JOB_ID;
 import static org.nuxeo.ai.adapters.DatasetExport.DATASET_EXPORT_TYPE;
 import static org.nuxeo.ai.bulk.BulkRemoveEnrichmentAction.PARAM_MODEL;
@@ -72,21 +66,16 @@ import static org.nuxeo.ai.bulk.BulkRemoveEnrichmentAction.PARAM_XPATHS;
 import static org.nuxeo.ai.enrichment.TestConfiguredStreamProcessors.waitForNoLag;
 import static org.nuxeo.ai.pipes.functions.PropertyUtils.CATEGORY_TYPE;
 import static org.nuxeo.ai.pipes.functions.PropertyUtils.TEXT_TYPE;
-import static org.nuxeo.ai.pipes.services.JacksonUtil.MAPPER;
 import static org.nuxeo.ecm.core.bulk.message.BulkStatus.State.COMPLETED;
 
 @RunWith(FeaturesRunner.class)
-@Features({ EnrichmentTestFeature.class, AutomationFeature.class, PlatformFeature.class, CoreBulkFeature.class,
+@Features({ EnrichmentTestFeature.class, PlatformFeature.class, CoreBulkFeature.class,
         RepositoryElasticSearchFeature.class })
 @Deploy("org.nuxeo.ai.ai-model")
-@Deploy("org.nuxeo.ecm.platform.video.convert")
-@Deploy("org.nuxeo.ecm.platform.video.core")
 @Deploy("org.nuxeo.ai.ai-core")
 @Deploy({ "org.nuxeo.ai.ai-core:OSGI-INF/recordwriter-test.xml", "org.nuxeo.ai.ai-model:OSGI-INF/bulk-test.xml" })
 @Deploy("org.nuxeo.elasticsearch.core.test:elasticsearch-test-contrib.xml")
-public class BulkEnrichmentTest {
-
-    public static final int NUM_OF_DOCS = 100;
+public class BulkEnrichmentTest extends BaseBulkEnrich {
 
     @Rule
     public WireMockRule wireMockRule = new WireMockRule(5089);
@@ -100,31 +89,11 @@ public class BulkEnrichmentTest {
     @Inject
     protected TransactionalFeature txFeature;
 
-    public DocumentModel setupTestData() {
-        DocumentModel testRoot = session.createDocumentModel("/", "bulkenrichtest", "Folder");
-        testRoot = session.createDocument(testRoot);
-        session.saveDocument(testRoot);
-
-        DocumentModel test = session.getDocument(testRoot.getRef());
-        for (int i = 0; i < NUM_OF_DOCS; ++i) {
-            DocumentModel doc = session.createDocumentModel(test.getPathAsString(), "doc" + i, "File");
-            doc.setPropertyValue("dc:title", "doc_" + i % 2);
-            if (i % 5 == 0) {
-                doc.setPropertyValue("dc:language", "en" + i);
-            }
-            session.createDocument(doc);
-        }
-
-        txFeature.nextTransaction();
-        return testRoot;
-    }
-
     @Test
     @Deploy("org.nuxeo.ai.ai-model:OSGI-INF/cloud-client-test.xml")
     public void testBulkEnrich() throws Exception {
-        DocumentModel testRoot = setupTestData();
         String nxql = String.format("SELECT * from Document WHERE ecm:parentId='%s' AND ecm:primaryType = 'File'",
-                testRoot.getId());
+                getRoot().getId());
         BulkCommand command = new BulkCommand.Builder(BulkEnrichmentAction.ACTION_NAME, nxql).user(
                 session.getPrincipal().getName()).repository(session.getRepositoryName()).build();
         submitAndAssert(command);
@@ -214,9 +183,8 @@ public class BulkEnrichmentTest {
     @Test
     @Deploy("org.nuxeo.ai.ai-model:OSGI-INF/cloud-client-test.xml")
     public void shouldNotBacktrackConfirmedValue() throws Exception {
-        DocumentModel testRoot = setupTestData();
         String nxql = String.format("SELECT * from Document WHERE ecm:parentId='%s' AND ecm:primaryType = 'File'",
-                testRoot.getId());
+                getRoot().getId());
         BulkCommand command = new BulkCommand.Builder(BulkEnrichmentAction.ACTION_NAME, nxql).user(
                 session.getPrincipal().getName()).repository(session.getRepositoryName()).build();
         submitAndAssert(command);
@@ -260,23 +228,11 @@ public class BulkEnrichmentTest {
         assertThat(workingDoc.getPropertyValue("dc:title")).isEqualTo("you");
     }
 
-    private List<AutoHistory> getAutoHistories(DocumentModel workingDoc) throws java.io.IOException {
-        Blob autoBlob = (Blob) workingDoc.getProperty(ENRICHMENT_SCHEMA_NAME, AUTO_HISTORY);
-        assertThat(autoBlob).isNotNull();
-
-        TypeReference<List<AutoHistory>> HISTORY_TYPE = new TypeReference<List<AutoHistory>>() {
-        };
-        List<AutoHistory> autoHistories = MAPPER.readValue(autoBlob.getByteArray(), HISTORY_TYPE);
-        assertThat(autoHistories).isNotEmpty();
-        return autoHistories;
-    }
-
     @Test
     @Deploy("org.nuxeo.ai.ai-model:OSGI-INF/cloud-client-test.xml")
     public void testBulkExportNoAutoFields() throws Exception {
-        DocumentModel testRoot = setupTestData();
         String nxql = String.format("SELECT * from Document where ecm:primaryType = 'File' AND ecm:parentId='%s' ",
-                testRoot.getId());
+                getRoot().getId());
         String nxql_lang = nxql + "AND dc:language IS NOT NULL";
         LogManager manager = Framework.getService(StreamService.class).getLogManager("bulk");
 
@@ -312,25 +268,5 @@ public class BulkEnrichmentTest {
 
         DocumentModelList datasetExports = exportService.getDatasetExports(session, commandId);
         assertThat(datasetExports).isNotEmpty();
-    }
-
-    protected void submitAndAssert(BulkCommand command) throws InterruptedException {
-        bulkService.submit(command);
-        assertTrue("Bulk action didn't finish", bulkService.await(command.getId(), Duration.ofSeconds(60)));
-        BulkStatus status = bulkService.getStatus(command.getId());
-        assertEquals(COMPLETED, status.getState());
-        assertEquals(NUM_OF_DOCS, status.getProcessed());
-    }
-
-    protected List<DocumentModel> getSomeDocuments(String nxql) {
-        DocumentModelList enriched = session.query(nxql, 20);
-        // Choose some random documents and confirm they are enriched.
-        List<DocumentModel> docs = new ArrayList<>();
-        docs.add(enriched.get(14));
-        docs.add(enriched.get(1));
-        docs.add(enriched.get(9));
-        docs.add(enriched.get(4));
-        docs.add(enriched.get(16));
-        return docs;
     }
 }
---
+++ nuxeo-ai-model/src/test/java/org/nuxeo/ai/model/serving/AnnotationConfirmTest.java
@@ -0,0 +1,111 @@
+package org.nuxeo.ai.model.serving;
+
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.nuxeo.ai.auto.AutoHistory;
+import org.nuxeo.ai.bulk.BaseBulkEnrich;
+import org.nuxeo.ai.bulk.BulkEnrichmentAction;
+import org.nuxeo.ai.bulk.BulkRemoveEnrichmentAction;
+import org.nuxeo.ai.enrichment.EnrichmentTestFeature;
+import org.nuxeo.ecm.automation.AutomationService;
+import org.nuxeo.ecm.automation.OperationContext;
+import org.nuxeo.ecm.automation.core.util.Properties;
+import org.nuxeo.ecm.automation.test.AutomationFeature;
+import org.nuxeo.ecm.core.api.CoreSession;
+import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.core.bulk.CoreBulkFeature;
+import org.nuxeo.ecm.core.bulk.message.BulkCommand;
+import org.nuxeo.ecm.core.bulk.message.BulkCommand.Builder;
+import org.nuxeo.ecm.platform.test.PlatformFeature;
+import org.nuxeo.elasticsearch.test.RepositoryElasticSearchFeature;
+import org.nuxeo.lib.stream.log.LogManager;
+import org.nuxeo.runtime.api.Framework;
+import org.nuxeo.runtime.stream.StreamService;
+import org.nuxeo.runtime.test.runner.Deploy;
+import org.nuxeo.runtime.test.runner.Features;
+import org.nuxeo.runtime.test.runner.FeaturesRunner;
+import org.nuxeo.runtime.test.runner.TransactionalFeature;
+
+import javax.inject.Inject;
+import java.util.List;
+
+import static java.lang.String.format;
+import static java.time.Duration.ofSeconds;
+import static org.assertj.core.api.Java6Assertions.assertThat;
+import static org.nuxeo.ai.bulk.BulkRemoveEnrichmentAction.PARAM_MODEL;
+import static org.nuxeo.ai.enrichment.TestConfiguredStreamProcessors.waitForNoLag;
+
+@RunWith(FeaturesRunner.class)
+@Features({ EnrichmentTestFeature.class, AutomationFeature.class, PlatformFeature.class, CoreBulkFeature.class,
+        RepositoryElasticSearchFeature.class })
+@Deploy("org.nuxeo.ai.ai-model")
+@Deploy("org.nuxeo.ai.ai-core")
+@Deploy({ "org.nuxeo.ai.ai-core:OSGI-INF/recordwriter-test.xml", "org.nuxeo.ai.ai-model:OSGI-INF/bulk-test.xml" })
+@Deploy("org.nuxeo.elasticsearch.core.test:elasticsearch-test-contrib.xml")
+public class AnnotationConfirmTest extends BaseBulkEnrich {
+
+    @Rule
+    public WireMockRule wireMockRule = new WireMockRule(5089);
+
+    @Inject
+    protected CoreSession session;
+
+    @Inject
+    protected AutomationService automationService;
+
+    @Inject
+    protected TransactionalFeature txf;
+
+    @Test
+    @Deploy("org.nuxeo.ai.ai-model:OSGI-INF/cloud-client-test.xml")
+    public void shouldNotBacktrackConfirmedValue() throws Exception {
+        DocumentModel testRoot = setupTestData();
+        String repo = session.getRepositoryName();
+        String principal = session.getPrincipal().getName();
+
+        String nxql = format("SELECT * from Document WHERE ecm:parentId='%s' AND ecm:primaryType = 'File'",
+                testRoot.getId());
+        BulkCommand command = new Builder(BulkEnrichmentAction.ACTION_NAME, nxql).user(principal)
+                                                                                 .repository(repo)
+                                                                                 .build();
+        submitAndAssert(command);
+
+        LogManager manager = Framework.getService(StreamService.class).getLogManager("bulk");
+        waitForNoLag(manager, "enrichment.in", "enrichment.in$SaveEnrichmentFunction", ofSeconds(5));
+        txf.nextTransaction();
+
+        List<DocumentModel> docs = getSomeDocuments(nxql);
+        assertThat(docs).isNotEmpty();
+
+        DocumentModel workingDoc = docs.get(0);
+        assertThat((String) workingDoc.getPropertyValue("dc:title")).isEqualTo("you");
+        List<AutoHistory> autoHistories = getAutoHistories(workingDoc);
+        assertThat(autoHistories).hasSize(2);
+
+        AutoHistory onTitle = autoHistories.stream()
+                                           .filter(h -> h.getProperty().equals("dc:title"))
+                                           .findFirst()
+                                           .orElse(null);
+        assertThat(onTitle).isNotNull();
+
+        OperationContext ctx = new OperationContext(session);
+        ctx.setInput(workingDoc);
+        Properties props = new Properties();
+        props.put("dc:title", "you");
+        ctx.put("properties", props);
+        automationService.run(ctx, AnnotationConfirm.ID);
+        txf.nextTransaction();
+
+        BulkCommand removed = new Builder(BulkRemoveEnrichmentAction.ACTION_NAME, nxql).user(principal)
+                                                                                       .repository(repo)
+                                                                                       .param(PARAM_MODEL,
+                                                                                               "descBulkModel")
+                                                                                       .build();
+        submitAndAssert(removed);
+
+        workingDoc = session.getDocument(workingDoc.getRef());
+        assertThat(workingDoc.getPropertyValue("dc:title")).isEqualTo("you");
+    }
+}
