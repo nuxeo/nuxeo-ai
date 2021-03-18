900f18840d3ae30adf8207562ea3761a95113312
Julien Carsique <jcarsique@nuxeo.com>
2021-03-18 21:12:12 +0100

    fix nuxeo-ai-internal version

--- tools/release.sh
+++ tools/release.sh
@@ -37,7 +37,8 @@ echo "RELEASE_VERSION=$RELEASE_VERSION" > release.properties
 echo "INCREMENT=$INCREMENT" >> release.properties
 echo "NEXT_VERSION=$NEXT_VERSION" >> release.properties
 
-mvn -V -B versions:set -DnewVersion="$RELEASE_VERSION" -DgenerateBackupPoms=false
+mvn -V -B versions:set versions:set-property -DnewVersion="$RELEASE_VERSION" -Dproperty=nuxeo.ai.version -DgenerateBackupPoms=false
+sed "s,\(<version>\)\(.*\)\(<\/version>\),\1$RELEASE_VERSION\3," nuxeo-ai-internal/pom.xml
 git add -u
 if [ "$DRY_RUN" = 'true' ]; then
     echo "Dry run: skip 'jx step next-version' and 'jx step changelog'"
@@ -50,7 +51,9 @@ fi
 # Not including the release tag in master-10.10 history
 git reset --hard "origin/$BRANCH"
 
-mvn -B versions:set -DnewVersion="${NEXT_VERSION}-SNAPSHOT" -DgenerateBackupPoms=false
+mvn -B versions:set versions:set-property -DnewVersion="${NEXT_VERSION}-SNAPSHOT" -Dproperty=nuxeo.ai.version -DgenerateBackupPoms=false
+sed "s,\(<version>\)\(.*\)\(<\/version>\),\1$NEXT_VERSION\3," nuxeo-ai-internal/pom.xml
+git add -u
 jx step next-version --version="$NEXT_VERSION"
 git commit -a -m"Post release ${RELEASE_VERSION}. Set version ${NEXT_VERSION}."
 if [ "$DRY_RUN" = 'true' ]; then
