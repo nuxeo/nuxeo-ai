21ccd7cfa042f5df45108166420a5f5f2f63a8ba
Julien Carsique <jcarsique@nuxeo.com>
2021-05-05 18:16:49 +0200

    fix(AICORE-361): release PR to downstream
    Invert commit order because dependency-matrix/matrix.yaml is modified when updating the pom.xml
    The second commit will fix dependency-matrix/matrix.yaml without modifying the pom.xml

--- tools/updatebot.sh
+++ tools/updatebot.sh
@@ -5,9 +5,9 @@ set -o pipefail
 
 VERSION=$1
 
-jx step create pr regex --regex '^(?m)\s+repo: nuxeo-ai\n.*\s*version: (.*)$' --version "${VERSION}" \
-  --files dependency-matrix/matrix.yaml \
-  --repo https://github.com/nuxeo/nuxeo-ai-integration.git --base master --branch master --dry-run="$DRY_RUN"
 jx step create pr regex --regex '(?m)ai-core-parent.*$\n^\s+<version>(.*)<\/version>$' --version "${VERSION}-SNAPSHOT" \
   --files pom.xml \
+  --repo https://github.com/nuxeo/nuxeo-ai-integration.git --base master --branch master --dry-run="$DRY_RUN"
+jx step create pr regex --regex '^(?m)\s+repo: nuxeo-ai\n.*\s*version: (.*)$' --version "${VERSION}" \
+  --files dependency-matrix/matrix.yaml \
   --repo https://github.com/nuxeo/nuxeo-ai-integration.git --base master --branch master --dry-run="$DRY_RUN"
