5339f542149729f5de3ddf1bf2b12da4cd1669a3
Julien Carsique <jcarsique@nuxeo.com>
2021-06-11 13:19:10 +0200

    style(INSIGHT-921): code format (other types)

--- addons/nuxeo-ai-image-quality-core/README.md
+++ addons/nuxeo-ai-image-quality-core/README.md
@@ -1,11 +1,16 @@
 # Nuxeo AI Image Quality
-An implementation of an enrichment service that uses [Sightengine](https://sightengine.com/). Based on the work done by [Joaquin Garzon](https://github.com/joaquinNX).
+
+An implementation of an enrichment service that uses [Sightengine](https://sightengine.com/). Based on the work done
+by [Joaquin Garzon](https://github.com/joaquinNX).
 
 ## Installation
+
 #### Quick start
+
 1. Install the nuxeo-ai-image-quality package. `./bin/nuxeoctl mp-install nuxeo-ai-image-quality`
 
 2. Add the following parameters to `nuxeo.conf`.
+
 ```
 nuxeo.ai.images.enabled=true
 nuxeo.enrichment.save.tags=true
@@ -15,9 +20,11 @@ nuxeo.ai.sightengine.apiKey=YOUR_API_KEY
 nuxeo.ai.sightengine.apiSecret=YOUR_API_SECRET
 ```
 
-3. Start Nuxeo and upload an image.  
+3. Start Nuxeo and upload an image.
 4. Wait 10 seconds then look at the document tags and document json `enrichment:items` facet.
+
 ### Configuration Parameters
+
 You can set these in your `nuxeo.conf`.
 <div class="table-scroll">
 <table class="hover">
@@ -50,10 +57,15 @@ You can set these in your `nuxeo.conf`.
 </table>
 </div>
 
-
 # License
+
 [Apache License, Version 2.0](http://www.apache.org/licenses/LICENSE-2.0.html)
 
 # About Nuxeo
 
-Nuxeo dramatically improves how content-based applications are built, managed and deployed, making customers more agile, innovative and successful. Nuxeo provides a next generation, enterprise ready platform for building traditional and cutting-edge content oriented applications. Combining a powerful application development environment with SaaS-based tools and a modular architecture, the Nuxeo Platform and Products provide clear business value to some of the most recognizable brands including Verizon, Electronic Arts, Netflix, Sharp, FICO, the U.S. Navy, and Boeing. Nuxeo is headquartered in New York and Paris. More information is available at www.nuxeo.com.
+Nuxeo dramatically improves how content-based applications are built, managed and deployed, making customers more agile,
+innovative and successful. Nuxeo provides a next generation, enterprise ready platform for building traditional and
+cutting-edge content oriented applications. Combining a powerful application development environment with SaaS-based
+tools and a modular architecture, the Nuxeo Platform and Products provide clear business value to some of the most
+recognizable brands including Verizon, Electronic Arts, Netflix, Sharp, FICO, the U.S. Navy, and Boeing. Nuxeo is
+headquartered in New York and Paris. More information is available at www.nuxeo.com.
--- tools/sprint.sh
+++ tools/sprint.sh
@@ -40,33 +40,33 @@ CONFIG_FILE=$GIT_TOP_LVL/.git/SPRINT
 hub sync || git fetch origin
 
 if [[ -f $CONFIG_FILE ]]; then
-    # shellcheck source=.git/SPRINT
-    source "$CONFIG_FILE"
+  # shellcheck source=.git/SPRINT
+  source "$CONFIG_FILE"
 else
-    PS3="Pick a number: "
-    mapfile -t options < <(git branch -r --list "origin/sprint*" | cut -f 2- -d /)
-    echo "Sprint to merge?"
-    select oldSprint in "${options[@]}"; do
-        if [[ -z $oldSprint ]]; then
-            echo "invalid answer $REPLY, choose a number"
-            continue
-        fi
-        break
-    done
-    echo
-    mapfile -t options < <(git branch -r --list "origin/master*" | cut -f 2- -d /)
-    echo "Base branch?"
-    select base in "${options[@]}"; do
-        if [[ -z $oldSprint ]]; then
-            echo "invalid answer $REPLY, choose a number"
-            continue
-        fi
-        break
-    done
-    echo
-    read -rp "New sprint name? " newSprint
+  PS3="Pick a number: "
+  mapfile -t options < <(git branch -r --list "origin/sprint*" | cut -f 2- -d /)
+  echo "Sprint to merge?"
+  select oldSprint in "${options[@]}"; do
+    if [[ -z $oldSprint ]]; then
+      echo "invalid answer $REPLY, choose a number"
+      continue
+    fi
+    break
+  done
+  echo
+  mapfile -t options < <(git branch -r --list "origin/master*" | cut -f 2- -d /)
+  echo "Base branch?"
+  select base in "${options[@]}"; do
+    if [[ -z $oldSprint ]]; then
+      echo "invalid answer $REPLY, choose a number"
+      continue
+    fi
+    break
+  done
+  echo
+  read -rp "New sprint name? " newSprint
 
-    cat >"$(git rev-parse --show-toplevel)/.git/SPRINT" <<EOF
+  cat >"$(git rev-parse --show-toplevel)/.git/SPRINT" <<EOF
 oldSprint=$oldSprint
 newSprint=$newSprint
 base=$base
@@ -78,66 +78,66 @@ echo "New Sprint:       $newSprint"
 echo "Base branch:      $base"
 read -p "Continue? " -n 1 -r
 if [[ ! $REPLY =~ ^[Yy]$ ]]; then
-    exit 1
+  exit 1
 fi
 echo
 
 function mergeOldSprint() {
-    if [[ -n $(git log "${oldSprint}..origin/${base}") ]]; then
-        echo "Rebase required"
-        #        oldSprintId=$(git rev-parse --short origin/"$oldSprint")
-        git checkout "$oldSprint"
-        git rebase "origin/$base"
-        git logone ...'@{u}'
-        git fetch origin
-        git id "origin/$oldSprint"
-        git status -sb
-        echo "Please review the rebase of $oldSprint onto $base, then force push and execute again sprint.sh"
-        #        echo "Please handle the following, then execute again sprint.sh:
-        #git push -f
-        #gh pr create -B "$base" -H "$oldSprint" -l "work in progress"
-        #gh pr checks
-        #git fetch origin
-        #git logone "origin/$oldSprint".. # must be empty: no commit on base that is missing on sprint
-        #gh pr merge -md
-        #"
-        exit
-    fi
+  if [[ -n $(git log "${oldSprint}..origin/${base}") ]]; then
+    echo "Rebase required"
+    #        oldSprintId=$(git rev-parse --short origin/"$oldSprint")
+    git checkout "$oldSprint"
+    git rebase "origin/$base"
+    git logone ...'@{u}'
+    git fetch origin
+    git id "origin/$oldSprint"
+    git status -sb
+    echo "Please review the rebase of $oldSprint onto $base, then force push and execute again sprint.sh"
+    #        echo "Please handle the following, then execute again sprint.sh:
+    #git push -f
+    #gh pr create -B "$base" -H "$oldSprint" -l "work in progress"
+    #gh pr checks
+    #git fetch origin
+    #git logone "origin/$oldSprint".. # must be empty: no commit on base that is missing on sprint
+    #gh pr merge -md
+    #"
+    exit
+  fi
 
-    echo "MERGE $oldSprint -> $base"
-    git checkout "$base"
-    git merge --no-ff --no-edit "$oldSprint"
-    git log --oneline '@{u}'..
-    echo
-    read -p "Push $base? " -n 1 -r
-    if [[ ! $REPLY =~ ^[Yy]$ ]]; then
-        exit 1
-    fi
-    echo
-    git push origin "$base"
+  echo "MERGE $oldSprint -> $base"
+  git checkout "$base"
+  git merge --no-ff --no-edit "$oldSprint"
+  git log --oneline '@{u}'..
+  echo
+  read -p "Push $base? " -n 1 -r
+  if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+    exit 1
+  fi
+  echo
+  git push origin "$base"
 
-    # TODO update nuxeo-ai-integration
+  # TODO update nuxeo-ai-integration
 }
 
 function createNewSprint() {
-    echo "CREATE $newSprint <- $base"
-    git checkout -b "$newSprint" "origin/$base" --no-track
-    currentVersion="$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)"
-    nextVersion="$(semver bump minor "$currentVersion")-SNAPSHOT"
-    mvn -V -B versions:set versions:set-property -DnewVersion="${nextVersion}" -Dproperty=nuxeo.ai.version -DgenerateBackupPoms=false
-    sed -i "s,version: .*,version: ${nextVersion}," charts/*/Chart.yaml
-    git commit -m"set version ${nextVersion}" -a
-    git log --oneline "$base"..
-    echo
-    read -p "Push new branch $newSprint and delete old $oldSprint? " -n 1 -r
-    if [[ ! $REPLY =~ ^[Yy]$ ]]; then
-        exit 1
-    fi
-    echo
-    git push --set-upstream origin "$newSprint:$newSprint"
-    git branch -d "$oldSprint"
-    git push --delete origin "$oldSprint"
-    rm "$CONFIG_FILE"
+  echo "CREATE $newSprint <- $base"
+  git checkout -b "$newSprint" "origin/$base" --no-track
+  currentVersion="$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)"
+  nextVersion="$(semver bump minor "$currentVersion")-SNAPSHOT"
+  mvn -V -B versions:set versions:set-property -DnewVersion="${nextVersion}" -Dproperty=nuxeo.ai.version -DgenerateBackupPoms=false
+  sed -i "s,version: .*,version: ${nextVersion}," charts/*/Chart.yaml
+  git commit -m"set version ${nextVersion}" -a
+  git log --oneline "$base"..
+  echo
+  read -p "Push new branch $newSprint and delete old $oldSprint? " -n 1 -r
+  if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+    exit 1
+  fi
+  echo
+  git push --set-upstream origin "$newSprint:$newSprint"
+  git branch -d "$oldSprint"
+  git push --delete origin "$oldSprint"
+  rm "$CONFIG_FILE"
 }
 
 mergeOldSprint
