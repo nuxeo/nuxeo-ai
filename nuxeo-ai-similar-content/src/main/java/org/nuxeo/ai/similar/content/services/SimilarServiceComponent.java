76bb77cd81323d32168aa3d3ecd7bd03d687ad0d
Andrei Nechaev <anechaev@nuxeo.com>
2021-10-20 16:51:36 +0200

    AICORE-542: dedup configuration extension and initial component

---
+++ nuxeo-ai-similar-content/src/main/java/org/nuxeo/ai/similar/content/services/SimilarServiceComponent.java
@@ -0,0 +1,54 @@
+/*
+ * (C) Copyright 2006-2021 Nuxeo (http://nuxeo.com/) and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *
+ * Contributors:
+ *    Andrei Nechaev
+ *
+ */
+package org.nuxeo.ai.similar.content.services;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.nuxeo.ai.similar.content.configuration.DeduplicationDescriptor;
+import org.nuxeo.runtime.model.ComponentInstance;
+import org.nuxeo.runtime.model.DefaultComponent;
+
+public class SimilarServiceComponent extends DefaultComponent implements SimilarContentService {
+
+    public static final String DEDUPLICATION_CONFIG_XP = "configuration";
+
+    protected final Map<String, DeduplicationDescriptor> dedupDescriptors = new HashMap<>();
+
+    @Override
+    public void registerContribution(Object contribution, String xp, ComponentInstance component) {
+        if (DEDUPLICATION_CONFIG_XP.equals(xp)) {
+            DeduplicationDescriptor desc = (DeduplicationDescriptor) contribution;
+            dedupDescriptors.put(desc.getName(), desc);
+        }
+    }
+
+    @Override
+    public String getQuery(String name) {
+        return dedupDescriptors.get(name).getQuery();
+    }
+
+    @Override
+    public String getXPath(String name) {
+        return dedupDescriptors.get(name).getXPath();
+    }
+}
