4234a7f1d17e34ae9a4c1d2d9a87fee436faa615
Vladimir Pasquier <vpasquier@nuxeo.com>
2021-11-17 13:06:01 -0500

    AICORE-527: add dedup listener

--- nuxeo-ai-similar-content/src/test/java/org/nuxeo/ai/similar/content/operations/DeduplicationListenersTest.java
+++ nuxeo-ai-similar-content/src/test/java/org/nuxeo/ai/similar/content/operations/DeduplicationListenersTest.java
@@ -24,29 +24,19 @@ package org.nuxeo.ai.similar.content.operations;
 import static com.github.tomakehurst.wiremock.client.WireMock.ok;
 import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
-import static org.assertj.core.api.Java6Assertions.assertThat;
 import static org.nuxeo.ai.pipes.functions.PropertyUtils.FILE_CONTENT;
 
 import java.io.Serializable;
-import java.util.HashMap;
-import java.util.Map;
-
 import javax.inject.Inject;
-import javax.ws.rs.core.Response;
-
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.nuxeo.ai.similar.content.operation.DedupDeleteIndexOperation;
-import org.nuxeo.ai.similar.content.operation.DedupIndexOperation;
-import org.nuxeo.ecm.automation.AutomationService;
-import org.nuxeo.ecm.automation.OperationContext;
-import org.nuxeo.ecm.automation.OperationException;
 import org.nuxeo.ecm.automation.test.AutomationFeature;
 import org.nuxeo.ecm.core.api.Blob;
 import org.nuxeo.ecm.core.api.Blobs;
 import org.nuxeo.ecm.core.api.CoreSession;
 import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.core.api.IdRef;
 import org.nuxeo.ecm.core.test.DefaultRepositoryInit;
 import org.nuxeo.ecm.core.test.annotations.Granularity;
 import org.nuxeo.ecm.core.test.annotations.RepositoryConfig;
@@ -54,11 +44,9 @@ import org.nuxeo.ecm.platform.test.PlatformFeature;
 import org.nuxeo.runtime.test.runner.Deploy;
 import org.nuxeo.runtime.test.runner.Features;
 import org.nuxeo.runtime.test.runner.FeaturesRunner;
-
 import com.github.tomakehurst.wiremock.client.WireMock;
 import com.github.tomakehurst.wiremock.extension.responsetemplating.ResponseTemplateTransformer;
 import com.github.tomakehurst.wiremock.junit.WireMockRule;
-import com.sun.jersey.core.spi.factory.ResponseImpl;
 
 @RunWith(FeaturesRunner.class)
 @Features({ PlatformFeature.class, AutomationFeature.class })
@@ -66,56 +54,39 @@ import com.sun.jersey.core.spi.factory.ResponseImpl;
 @Deploy("org.nuxeo.ai.nuxeo-jwt-authenticator-core")
 @Deploy("org.nuxeo.ai.ai-model")
 @Deploy("org.nuxeo.ai.similar-content-test:OSGI-INF/cloud-client-datasource-test.xml")
-@Deploy("org.nuxeo.ai.similar-content-test:OSGI-INF/disable-dedup-listener.xml")
 @RepositoryConfig(init = DefaultRepositoryInit.class, cleanup = Granularity.METHOD)
-public class DeduplicationOperationsTest {
-
-    public static final String XPATH = "file:content";
+public class DeduplicationListenersTest {
 
     @Rule
     public WireMockRule wireMockRule = new WireMockRule(
             options().extensions(new ResponseTemplateTransformer(true)).port(5089));
 
     @Inject
-    protected AutomationService automationService;
-
-    @Inject
     protected CoreSession session;
 
     @Test
-    public void iCanUseIndexOperation() throws OperationException {
-        OperationContext ctx = new OperationContext(session);
-
+    public void shouldManageIndexFromListener() {
         Blob textBlob = Blobs.createBlob("this is a blob");
         DocumentModel fileDoc = session.createDocumentModel("/", "TestFile", "File");
         fileDoc.setPropertyValue(FILE_CONTENT, (Serializable) textBlob);
         fileDoc = session.createDocument(fileDoc);
-        session.save();
 
-        String url = "/api/v1/ai/dedup/mockTestProject/index/" + fileDoc.getId() + "/" + FILE_CONTENT;
-        stubFor(WireMock.post(url).willReturn(ok()));
-
-        ctx.setInput(fileDoc);
-        Map<String, Object> params = new HashMap<>();
-        params.put("xpath", XPATH);
-        ResponseImpl response = (ResponseImpl) automationService.run(ctx, DedupIndexOperation.ID, params);
-        assertThat(response.getStatus()).isEqualTo(Response.Status.OK.getStatusCode());
-    }
-
-    @Test
-    public void iCanUseDeleteIndexOperation() throws OperationException {
-        DocumentModel fileDoc = session.createDocumentModel("/", "TestFile", "File");
-        fileDoc = session.createDocument(fileDoc);
-        session.save();
-
+        // When the doc blob is deleted
         String url = "/api/v1/ai/dedup/mockTestProject/index/" + fileDoc.getId() + "?xpath=" + FILE_CONTENT;
         stubFor(WireMock.delete(url).willReturn(ok()));
 
-        OperationContext ctx = new OperationContext(session);
-        ctx.setInput(fileDoc);
-        Map<String, Object> params = new HashMap<>();
-        params.put("xpath", XPATH);
-        ResponseImpl response = (ResponseImpl) automationService.run(ctx, DedupDeleteIndexOperation.ID, params);
-        assertThat(response.getStatus()).isEqualTo(Response.Status.OK.getStatusCode());
+        // When the doc is deleted
+        url = "/api/v1/ai/dedup/mockTestProject/index/" + fileDoc.getId();
+        stubFor(WireMock.delete(url).willReturn(ok()));
+
+        textBlob = Blobs.createBlob("this is another blob");
+        fileDoc.setPropertyValue(FILE_CONTENT, (Serializable) textBlob);
+        fileDoc = session.saveDocument(fileDoc);
+
+        fileDoc.setPropertyValue(FILE_CONTENT, null);
+        fileDoc = session.saveDocument(fileDoc);
+
+        session.removeDocument(new IdRef(fileDoc.getId()));
     }
+
 }
