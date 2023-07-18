816984a8bad41a96e48e3b77f432784f22f98faa
Andrei Nechaev <andrei.nechaev@hyland.com>
2023-07-18 16:11:21 +0200

    AICORE-624: update ES to Nx 2023

--- nuxeo-ai-core/src/main/java/org/nuxeo/ai/services/ModelUsageServiceImpl.java
+++ nuxeo-ai-core/src/main/java/org/nuxeo/ai/services/ModelUsageServiceImpl.java
@@ -1,21 +1,24 @@
 package org.nuxeo.ai.services;
 
 import java.io.IOException;
+import org.apache.http.util.EntityUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.nuxeo.ai.AIConstants;
 import org.nuxeo.ecm.core.api.CoreSession;
-import org.nuxeo.elasticsearch.http.readonly.HttpClient;
+import org.nuxeo.elasticsearch.api.ESClient;
+import org.nuxeo.elasticsearch.api.ElasticSearchAdmin;
+import org.nuxeo.elasticsearch.client.ESRestClient;
 import org.nuxeo.elasticsearch.http.readonly.filter.DefaultSearchRequestFilter;
 import org.nuxeo.elasticsearch.http.readonly.filter.SearchRequestFilter;
 import org.nuxeo.elasticsearch.http.readonly.service.RequestFilterService;
 import org.nuxeo.runtime.api.Framework;
+import org.opensearch.client.Request;
+import org.opensearch.client.Response;
 
 public class ModelUsageServiceImpl implements ModelUsageService {
 
     private static final Logger log = LogManager.getLogger(ModelUsageServiceImpl.class);
-
-    protected static final String DEFAULT_ES_BASE_URL = "http://localhost:9200/";
 
     protected static final String INDICES = "audit";
 
@@ -58,25 +61,29 @@ public class ModelUsageServiceImpl implements ModelUsageService {
     public String usage(CoreSession session, AIConstants.AUTO type, String modelId) {
         RequestFilterService requestFilterService = Framework.getService(RequestFilterService.class);
         try {
-            SearchRequestFilter req = requestFilterService.getRequestFilters(INDICES);
-            if (req == null) {
-                req = new DefaultSearchRequestFilter();
+            SearchRequestFilter filter = requestFilterService.getRequestFilters(INDICES);
+            if (filter == null) {
+                filter = new DefaultSearchRequestFilter();
             }
 
             String payload = String.format(AGGREGATE_BY_DATE_TEMPL, type.eventName(), modelId);
-            req.init(session, INDICES, RAW_QUERY, payload);
-            log.debug(req);
-            return HttpClient.get(getElasticsearchBaseUrl() + req.getUrl(), req.getPayload());
+
+            filter.init(session, INDICES, RAW_QUERY, payload);
+            log.debug(filter);
+
+            ESClient esClient = Framework.getService(ElasticSearchAdmin.class).getClient();
+            if (!(esClient instanceof ESRestClient client)) {
+                throw new IllegalStateException("Passthrough works only with a RestClient");
+            }
+            Request request = new Request("GET", filter.getUrl());
+            if (payload != null) {
+                request.setJsonEntity(payload);
+            }
+            Response response = client.performRequestWithTracing(request);
+            return EntityUtils.toString(response.getEntity());
         } catch (ReflectiveOperationException | IOException e) {
             log.error("Error when trying to get Search Request Filter for indices {}", INDICES, e);
             return null;
         }
-    }
-
-    protected String getElasticsearchBaseUrl() {
-        if (esBaseUrl == null) {
-            esBaseUrl = Framework.getProperty(ES_BASE_URL_PROPERTY, DEFAULT_ES_BASE_URL);
-        }
-        return esBaseUrl;
     }
 }
